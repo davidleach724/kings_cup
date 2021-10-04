862b4465b18ddad16a2ccede3b8872760d55e37f
David Leach <davidleach724@gmail.com>
2021-10-04 10:39:45 -0600

    Generate random card order

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react'
 import './Game.css'
 
 const Game = () => {
+  const [cardOrder, setCardOrder] = useState([])
+  const [currentCard, setCurrentCard] = useState({})
+
+  useEffect(() => {
+    setCardOrder(generateCardOrder())
+  }, [])
+
+  const generateCardOrder = () => {
+    let orderList = []
+    while (orderList.length != 50) {
+      let num = Math.floor(Math.random() * 50);
+      if (!orderList.includes(num)) {
+        orderList.push(num)
+      }
+    }
+    return orderList;
+  }
+
   return (
-    <h1>This is the game</h1>
+    <section>
+      <h1>This is the game</h1>
+      <button>DRAW</button>
+    </section>
   )
 }
 
