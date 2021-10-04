bda8d6bad2c349d3182095081b0717b6ace2e54b
David Leach <davidleach724@gmail.com>
2021-10-04 14:07:16 -0600

    Create card data

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -1,10 +1,210 @@
 export const cardData = [
   {
     name: 'club_1',
-    rule: 'Ace: Waterfall'
+    rule: ` Ace is for waterfall (everyone starts drinking and doesn't stop until the person before them stops)`
   },
   {
     name: 'club_2',
-    rule: '2 is for you'
+    rule: '2 is for you (choose a person)'
+  },
+  {
+    name: 'club_3',
+    rule: '3 is for me (you drink)'
+  },
+  {
+    name: 'club_4',
+    rule: '4 is for ... the women (women drink)'
+  },
+  {
+    name: 'club_5',
+    rule: '5 is for never have I ever (one round, losers drink)'
+  },
+  {
+    name: 'club_6',
+    rule: '6 is for ... the men'
+  },
+  {
+    name: 'club_7',
+    rule: '7 is for heaven (last person pointing up drinks)'
+  },
+  {
+    name: 'club_8',
+    rule: '8 is for mate (choose a partner to drink with)'
+  },
+  {
+    name: 'club_9',
+    rule: '9 is for rhyme (rhyme a word or phrase and whoever can\'t drinks)'
+  },
+  {
+    name: 'club_10',
+    rule: '10 is for categories (start a category, whoever can\'t list something drinks)'
+  },
+  {
+    name: 'club_jack',
+    rule: 'Jack is thumb master (place your thumb on the table, last one drinks)'
+  },
+  {
+    name: 'club_queen',
+    rule: 'Queen is question master (whoever answers your question drinks)'
+  },
+  {
+    name: 'club_king',
+    rule: 'King is for rules (create a new rule, whoever breaks it drinks)'
+  },
+  {
+    name: 'diamond_1',
+    rule: ` Ace is for waterfall (everyone starts drinking and doesn't stop until the person before them stops)`
+  },
+  {
+    name: 'diamond_2',
+    rule: '2 is for you (choose a person)'
+  },
+  {
+    name: 'diamond_3',
+    rule: '3 is for me (you drink)'
+  },
+  {
+    name: 'diamond_4',
+    rule: '4 is for ... the women (women drink)'
+  },
+  {
+    name: 'diamond_5',
+    rule: '5 is for never have I ever (one round, losers drink)'
+  },
+  {
+    name: 'diamond_6',
+    rule: '6 is for ... the men'
+  },
+  {
+    name: 'diamond_7',
+    rule: '7 is for heaven (last person pointing up drinks)'
+  },
+  {
+    name: 'diamond_8',
+    rule: '8 is for mate (choose a partner to drink with)'
+  },
+  {
+    name: 'diamond_9',
+    rule: '9 is for rhyme (rhyme a word or phrase and whoever can\'t drinks)'
+  },
+  {
+    name: 'diamond_10',
+    rule: '10 is for categories (start a category, whoever can\'t list something drinks)'
+  },
+  {
+    name: 'diamond_jack',
+    rule: 'Jack is thumb master (place your thumb on the table, last one drinks)'
+  },
+  {
+    name: 'diamond_queen',
+    rule: 'Queen is question master (whoever answers your question drinks)'
+  },
+  {
+    name: 'diamond_king',
+    rule: 'King is for rules (create a new rule, whoever breaks it drinks)'
+  },
+  {
+    name: 'heart_1',
+    rule: ` Ace is for waterfall (everyone starts drinking and doesn't stop until the person before them stops)`
+  },
+  {
+    name: 'heart_2',
+    rule: '2 is for you (choose a person)'
+  },
+  {
+    name: 'heart_3',
+    rule: '3 is for me (you drink)'
+  },
+  {
+    name: 'heart_4',
+    rule: '4 is for ... the women (women drink)'
+  },
+  {
+    name: 'heart_5',
+    rule: '5 is for never have I ever (one round, losers drink)'
+  },
+  {
+    name: 'heart_6',
+    rule: '6 is for ... the men'
+  },
+  {
+    name: 'heart_7',
+    rule: '7 is for heaven (last person pointing up drinks)'
+  },
+  {
+    name: 'heart_8',
+    rule: '8 is for mate (choose a partner to drink with)'
+  },
+  {
+    name: 'heart_9',
+    rule: '9 is for rhyme (rhyme a word or phrase and whoever can\'t drinks)'
+  },
+  {
+    name: 'heart_10',
+    rule: '10 is for categories (start a category, whoever can\'t list something drinks)'
+  },
+  {
+    name: 'heart_jack',
+    rule: 'Jack is thumb master (place your thumb on the table, last one drinks)'
+  },
+  {
+    name: 'heart_queen',
+    rule: 'Queen is question master (whoever answers your question drinks)'
+  },
+  {
+    name: 'heart_king',
+    rule: 'King is for rules (create a new rule, whoever breaks it drinks)'
+  },
+  {
+    name: 'spade_1',
+    rule: ` Ace is for waterfall (everyone starts drinking and doesn't stop until the person before them stops)`
+  },
+  {
+    name: 'spade_2',
+    rule: '2 is for you (choose a person)'
+  },
+  {
+    name: 'spade_3',
+    rule: '3 is for me (you drink)'
+  },
+  {
+    name: 'spade_4',
+    rule: '4 is for ... the women (women drink)'
+  },
+  {
+    name: 'spade_5',
+    rule: '5 is for never have I ever (one round, losers drink)'
+  },
+  {
+    name: 'spade_6',
+    rule: '6 is for ... the men'
+  },
+  {
+    name: 'spade_7',
+    rule: '7 is for heaven (last person pointing up drinks)'
+  },
+  {
+    name: 'spade_8',
+    rule: '8 is for mate (choose a partner to drink with)'
+  },
+  {
+    name: 'spade_9',
+    rule: '9 is for rhyme (rhyme a word or phrase and whoever can\'t drinks)'
+  },
+  {
+    name: 'spade_10',
+    rule: '10 is for categories (start a category, whoever can\'t list something drinks)'
+  },
+  {
+    name: 'spade_jack',
+    rule: 'Jack is thumb master (place your thumb on the table, last one drinks)'
+  },
+  {
+    name: 'spade_queen',
+    rule: 'Queen is question master (whoever answers your question drinks)'
+  },
+  {
+    name: 'spade_king',
+    rule: 'King is for rules (create a new rule, whoever breaks it drinks)'
   }
 ]
