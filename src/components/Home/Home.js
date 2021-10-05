c80bf65503752d2747ef45b263bdb43e207dafcb
David Leach <davidleach724@gmail.com>
2021-10-05 08:42:32 -0600

    Swap out buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,7 @@
+import newGame from '../../images/new_game.png'
+import rules from '../../images/rules.png'
+import banner from '../../images/banner.png'
+
 import { Link } from "react-router-dom"
 import './Home.css'
 
@@ -7,10 +11,10 @@ const Home = () => {
     <section className="home-menu">
       <h1 className="game-title">Kings Cup</h1>
       <Link to="/game">
-        <button>New Game</button>
+        <img className="menu-btn" src={newGame} />
       </Link>
       <Link to="/rules">
-        <button>Rules</button>
+        <img className="menu-btn" src={rules} />
       </Link>
     </section>
   )
