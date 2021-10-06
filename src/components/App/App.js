96aef55237d52e12916071ecade086ab08608ff4
David Leach <davidleach724@gmail.com>
2021-10-06 11:35:59 -0600

    Deploy with HashRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
+import { HashRouter } from 'react-router-dom'
 import Game from '../Game/Game'
 import Home from '../Home/Home'
 import Rules from '../Rules/Rules'
@@ -9,12 +10,12 @@ const App = () => {
 
   return ( 
     <main>
-      <Switch>
-        <Route exact path='/' component={ Home } />
-        <Route path='/game' component={ Game } />
-        <Route path='/rules' component={ Rules } />
-        <Route component={ Home } />
-      </Switch>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/game' component={ Game } />
+          <Route path='/rules' component={ Rules } />
+          <Route component={ Home } />
+        </Switch>
     </main>
   )
 }
